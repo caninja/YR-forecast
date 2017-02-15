42f38c4c68978dcd4d2c8b9d6c216e6d30b3838a
Fredrik <caninja@users.noreply.github.com>
2017-02-15 14:31:22 +0100

    PHP Caching.
    Nå med mulighet for å cache med PHP, istede for crontab.

--- index.php
+++ index.php
@@ -1,47 +1,53 @@
 <?PHP
 $Betaling = "Værvarsel fra Yr levert av Meteorologisk institutt og NRK";
-# !! YR vil at du henter XML filen kun 1 gang per 10min, gjøres via cron
-# */10 * * * * wget http://www.yr.no -O /en/plass/yr.xml >/dev/null 2>&1
+# !! YR vil at du max henter XML filen 1 gang per 10min.
+# den sikreste måten og gjøre det på er via cron, se eksempel under.
+# */10 * * * * wget http://www.yr.no/varsel.xml -O /en/plass/varsel.xml
+# på den måten kan du scripte videre på et eget lokalt arkiv\ statestikk.
 ########################################################################
 function yr($vare = '')
 {
-	# Laster inn XML fra URL.
-	$url = "http://www.yr.no/sted/Norge/Oppland/Lillehammer/Lillehammer/varsel.xml" ; 
-	$xml = simplexml_load_file($url) or die("en hel feil skjedde.");
-  
-	# XML analysering til variabler.
-	$sted = $xml->location[0]->name;
-	$dato = $xml->forecast->tabular->time[$vare]['from'];
-	$periode = $xml->forecast->tabular->time[$vare]['period'];
-	$symbol = $xml->forecast->tabular->time[$vare]->symbol['name'];
-	$symbolverdi = $xml->forecast->tabular->time[$vare]->symbol['var'];
-	$temperature = $xml->forecast->tabular->time[$vare]->temperature['value'];
-	$windSpeedName = $xml->forecast->tabular->time[$vare]->windSpeed['name'];
-	$windSpeedMps = $xml->forecast->tabular->time[$vare]->windSpeed['mps'];
-	$windSpeedDirection = $xml->forecast->tabular->time[$vare]->windDirection['name'];
-	$precipitation = $xml->forecast->tabular->time[$vare]->precipitation['value'];
-	
-	# Fikse på et felt for å hente ut en ren verdi til slutt.
-	$dato = $dato2 = substr($dato, 0, 10);
+        $url="http://www.yr.no/sted/Norge/Oppland/Lillehammer/Lillehammer/varsel.xml";
+        $cache="varsel.xml";
 
-	# Liten fiks for varierende tid.
-	if ($periode == 0)/*_*/{$periode = "00:00 - 06:00";} 
-	elseif ($periode == 1) {$periode = "06:00 - 12:00";}
-	elseif ($periode == 2) {$periode = "12:00 - 18:00";} 
-	/****************/else {$periode = "18:00 - 00:00";}
+        # Hent varsel.xml max 1 gang per 10min
+        if (file_exists($cache)&&(filemtime($cache)>(time()-600))){
+        } else {file_put_contents($cache,fopen($url,'r'));}
 
-	# HTML
-	echo "<div class=\"divTableRow\">";
-	echo "<div class=\"divTableCell\"><b>$dato</b></div>";
-	echo "<div class=\"divTableCell\"><b>$periode</b></div>";
-	echo "<div class=\"divTableCell\"><img src=\"https://symbol.yr.no/grafikk/sym/b38/$symbolverdi.png\" /></div>"; //Bruker YR sine symboler.
-	echo "<div class=\"divTableCell\">$precipitation mm nedbør</div>";
-	echo "<div class=\"divTableCell\">$temperature °</div>";
-	echo "<div class=\"divTableCell\">$windSpeedName fra $windSpeedDirection</div>";
-	echo "<div class=\"divTableCell\">$windSpeedMps m/s</div>";
-	echo "</div>";
+        # Last inn og analyser XML.
+        $xml          =simplexml_load_file($cache)or die("Feil #1.");
+        $dato         =$xml->forecast->tabular->time[$vare]['from'];
+        $periode      =$xml->forecast->tabular->time[$vare]['period'];
+        $symbol       =$xml->forecast->tabular->time[$vare]->symbol['name'];
+        $symbolverdi  =$xml->forecast->tabular->time[$vare]->symbol['var'];
+        $temperature  =$xml->forecast->tabular->time[$vare]->temperature['value'];
+        $windSpeedName=$xml->forecast->tabular->time[$vare]->windSpeed['name'];
+        $windSpeedMps =$xml->forecast->tabular->time[$vare]->windSpeed['mps'];
+        $windSpeedDir =$xml->forecast->tabular->time[$vare]->windDirection['name'];
+        $precipitation=$xml->forecast->tabular->time[$vare]->precipitation['value'];
+
+        # Fikse på et felt for å hente ut en ren verdi til slutt.
+        $dato = $dato2 = substr($dato, 0, 10);
+
+        # Liten fiks for varierende tid.
+        if     ($periode == 0) {$periode="00:00 - 06:00";}
+        elseif ($periode == 1) {$periode="06:00 - 12:00";}
+        elseif ($periode == 2) {$periode="12:00 - 18:00";}
+        /****************/else {$periode="18:00 - 00:00";}
+
+        # HTML
+        echo "<div class=\"divTableRow\">";
+        echo "<div class=\"divTableCell\"><b>$dato</b></div>";
+        echo "<div class=\"divTableCell\"><b>$periode</b></div>";
+        echo "<div class=\"divTableCell\"><img src=\"https://symbol.yr.no/grafikk/sym/b38/$symbolverdi.png\" /></div>"; //Bruker YR sine symboler.
+        echo "<div class=\"divTableCell\">$precipitation mm nedbør</div>";
+        echo "<div class=\"divTableCell\">$temperature °</div>";
+        echo "<div class=\"divTableCell\">$windSpeedName fra $windSpeedDir</div>";
+        echo "<div class=\"divTableCell\">$windSpeedMps m/s</div>";
+        echo "</div>";
 }
 ########################################################################
+
 ?>
 
 <html><head><title><?PHP echo $Betaling; ?></title>
